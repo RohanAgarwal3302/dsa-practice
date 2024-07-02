041cd6d018e0923ba674bb27b51fc7217a76a7e4
RohanAgarwal3302 <rohan.ra2002@gmail.com>
2024-07-02 18:05:30 +0530

    tweals

--- get_concatenation.py
+++ get_concatenation.py
@@ -1,3 +1,17 @@
+# Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+# Specifically, ans is the concatenation of two nums arrays.
+
+# Return the array ans.
+
+# nums = [1,2,1]
+# ans: [1,2,1,1,2,1]
+# Explanation: The array ans is formed as follows:
+# - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+# - ans = [1,2,1,1,2,1]
+
+# complete
+
 def getConcatenation(nums: list[int]):
         i=0
         ans=[]
@@ -20,3 +34,5 @@ def getConcatenation(nums: list[int]):
 def getConcatenation(nums: list[int]):
       nums.extend(nums)
       return nums
+  
+print(getConcatenation([1,2,1]))
