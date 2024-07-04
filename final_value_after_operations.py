1dd419ae672ca8172ee845e6d7ff97b836e86754
RohanAgarwal3302 <rohan.ra2002@gmail.com>
2024-07-04 12:58:21 +0530

    tweak

--- final_value_after_operations.py
+++ final_value_after_operations.py
@@ -1,8 +1,39 @@
+# There is a programming language with only four operations and one variable X:
+
+# ++X and X++ increments the value of the variable X by 1.
+# --X and X-- decrements the value of the variable X by 1.
+# Initially, the value of X is 0.
+
+# Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+# operations = ["--X","X++","X++"]
+# ans: 1
+# Explanation: The operations are performed as follows:
+# Initially, X = 0.
+# --X: X is decremented by 1, X =  0 - 1 = -1.
+# X++: X is incremented by 1, X = -1 + 1 =  0.
+# X++: X is incremented by 1, X =  0 + 1 =  1.
+
+# complete
+
 def finalValueAfterOperations(operations: list[str]):
-        x=0
-        for v in operations:
-            if v=="X++" or v=="++X":
-                x+=1
-            if v=="--X" or v=="X--":
-                x-=1
-        return x
+    x=0
+    for v in operations:
+        if v=="X++" or v=="++X":
+            x+=1
+        if v=="--X" or v=="X--":
+            x-=1
+    return x
+
+import re
+
+def finalValueAfterOperations(operations: list[str]):
+    sum
+    for op in operations:
+        if re.search(r'\B-',op):
+            sum-=1
+        else:
+            sum+=1
+    return sum
+    
+print(finalValueAfterOperations(["--X","++X","X--"]))
--- left_right_sum_diff.py
+++ left_right_sum_diff.py
@@ -1,3 +1,20 @@
+# Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+
+# answer.length == nums.length.
+# answer[i] = |leftSum[i] - rightSum[i]|.
+# Where:
+
+# leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+# rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+# Return the array answer.
+
+# nums = [10,4,8,3]
+# ans: [15,1,11,22]
+# Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+# The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+
+# completed
+
 def leftRigthDifference(nums:list[int]):
         r=0
         l=0
@@ -11,5 +28,16 @@ def leftRigthDifference(nums:list[int]):
             l+=nums[i-1]
             ans.append(abs(l-r))
         return ans
+    
+def leftRigthDifference(nums:list[int]):
+    lsum=0
+    rsum=sum(nums)
+    ans=[]
+    for i in range(len(nums)):
+        if i>0:
+            lsum+=nums[i-1]
+        rsum-=nums[i]
+        ans.append(abs(lsum-rsum))
+    return ans
 
 print(leftRigthDifference([10,4,8,3]))
