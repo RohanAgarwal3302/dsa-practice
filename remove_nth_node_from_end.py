1fd66ea2ee2ee62a9e21d2a2b4d5fb5508b0aedd
RohanAgarwal3302 <rohan.ra2002@gmail.com>
2025-02-07 14:09:38 +0530

    tweaked

--- remove_nth_node_from_end.py
+++ remove_nth_node_from_end.py
@@ -1,14 +1,16 @@
 # Given the head of a linked list, 
 # remove the nth node from the end of the list and return its head.
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 # head = [1,2,3,4,5], n = 2
 # ans: [1,2,3,5]
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 
 # completed
 
